da31a4565689214c104e89ee5c7cbf5e46ab80c4
enis75079 <naveenislam55@gmail.com>
2024-11-15 20:58:53 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -15,6 +15,16 @@ const double LEAVE = 0.55;
 const double JOIN = 0.45;
 
 int main() {
+    const int INIT_SIZE = 2;
+    deque<Car> tollLane;
+
+    for (int i = 0; i < INIT_SIZE; i++) {
+        tollLane.push_back(Car());
+    }
+
+    for (auto cars : tollLane) {
+        cars.print();
+    }
 
     return 0;
 }
