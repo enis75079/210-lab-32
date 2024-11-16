896cd4194dabc2cfa608e5e5807dd6ca3080a4b3
enis75079 <naveenislam55@gmail.com>
2024-11-16 14:33:03 -0800

    update 3

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main() {
     // intital queue before the while loop begins
     cout << "Initial Queue:" << endl;
     for (int i = 0; i < INIT_SIZE; i++) {
-        cout << "Lane " << i << ": " ;
+        cout << "Lane " << i + 1 << ":" << endl;
         for (auto car : tollLane[i]) {
             car.print();
         }
@@ -50,33 +50,21 @@ int main() {
     int n = 0;
 
     // loop until the time value equals 20 ie. runs the simulation for 20 time periods
-    /*while (n != 20) {
+    while (n != 20) {
         cout << "Time: " << n << " ";
-        //creates a random number that determines whether the car objects will leave or join
-        double randNum = (double)rand() / RAND_MAX;
         
-        // randNum between 0.0 to 1.0 that determines if the car leaves the toll
-        if (randNum <= LEAVE) {
-            cout << "Operation: Car paid: ";
-            tollLane.front().print();
-            tollLane.pop_front();
-        // if the randNum is apart of the 45%, create a new Car object that joins the deque
-        } else {
-            cout << "Operation: Joined lane: ";
-            Car nCar;
-            tollLane.push_back(nCar);
-            nCar.print();
+        for (int i = 0; i < INIT_SIZE; i++) {
+            //creates a random number that determines whether the car objects will leave or join
+            double randNum = (double)rand() / RAND_MAX;
+
+            // 46% probability: Car leaves
+            if (randNum <= LEAVE) {
+                cout << "Operation: Car"
+            }
         }
-        // output the current queue
-        cout << "Queue:" << endl;
-        for (auto cars : tollLane) {
-            cars.print();
-        }
-        cout << endl;
-        // increment the time value
-        n++;
+        
     }
-    cout << "Empty" << endl;*/
+    cout << "Empty" << endl;
 
     return 0;
 }
