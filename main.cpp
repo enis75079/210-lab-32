6fee123c601f5db4d7191e1ed845a461873542ed
enis75079 <naveenislam55@gmail.com>
2024-11-15 21:06:52 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -9,12 +9,15 @@ Naveen Islam
 #include <iostream>
 #include "Car.h"
 #include <deque>
+#include <ctime>
 using namespace std;
 
 const double LEAVE = 0.55;
 const double JOIN = 0.45;
 
 int main() {
+    srand(time(0));
+
     const int INIT_SIZE = 2;
     deque<Car> tollLane;
 
@@ -22,8 +25,17 @@ int main() {
         tollLane.push_back(Car());
     }
 
+    cout << "Initial Queue:" << endl;
     for (auto cars : tollLane) {
         cars.print();
+    }
+    cout << endl;
+
+    int n = 0;
+    while (tollLane.empty() != false) {
+        cout << "Time: " << n << endl;
+        
+        n++;
     }
 
     return 0;
