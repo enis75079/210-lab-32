f3e5df591d9311b14302cc3abb81e782c7b6c023
enis75079 <naveenislam55@gmail.com>
2024-11-15 20:53:43 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,20 @@
 /*
+
+lab32: toll booth
+COMSC-210
+Naveen Islam
+
  */
+
+#include <iostream>
+#include "Car.h"
+#include <deque>
+using namespace std;
+
+const double LEAVE = 0.55;
+const double JOIN = 0.45;
+
+int main() {
+
+    return 0;
+}
